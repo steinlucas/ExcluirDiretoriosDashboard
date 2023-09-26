5c702a22e8efeacc8f0b54a287bbc712de103e7f
Lucas Stein <66976832+steinlucas@users.noreply.github.com>
2023-09-26 15:11:45 -0300

    Update ExcluirDiretoriosInutilizados.bat

--- ExcluirDiretoriosInutilizados.bat
+++ ExcluirDiretoriosInutilizados.bat
@@ -1,10 +1,24 @@
 @echo off
 
+FOR /D %%a in (*) DO (
+     echo Excluindo diretorio %cd%\%%a\Dados\BinarioPlano
+     rd /s /q %cd%\%%a\Dados\BinarioPlano
+
+     echo Excluindo diretorio %cd%\%%a\Dados\BinarioEfetivacao
+     rd /s /q %cd%\%%a\Dados\BinarioEfetivacao
+
+     echo Excluindo diretorio %cd%\%%a\Dados\BinarioOcorrenciaReceita
+     rd /s /q %cd%\%%a\Dados\BinarioOcorrenciaReceita
+)
+
 FOR /D %%b in (*) DO (
     cd %cd%\%%b\versao
 
     FOR /D %%c in (*00.*) DO (
+	echo Excluindo diretorio %cd%\%%b\versao\%%c\dashboard
 	rd /s /q %cd%\%%b\versao\%%c\dashboard
+
+	echo Excluindo diretorio %cd%\%%b\versao\%%c\ReportBuilder
         rd /s /q %cd%\%%b\versao\%%c\ReportBuilder
     )
 )
