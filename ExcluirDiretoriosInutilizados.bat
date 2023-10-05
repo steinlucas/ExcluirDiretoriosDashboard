24a894ce15da1d61e4187bff40f26bbb4c365a0e
lucas.stein <stein.lucas@icloud.com>
2023-10-05 16:22:51 -0300

    Add VersaoPlano path

--- ExcluirDiretoriosInutilizados.bat
+++ ExcluirDiretoriosInutilizados.bat
@@ -5,7 +5,7 @@ FOR /D %%a in (Q:\*,
                R:\Process\*,
                R:\ORACLE19\SGT\*,
                R:\ORACLE19\Process\*,
-               P:\Cliente\Ora\*) DO (
+               P:\Cliente\Ora\* ) DO (
 
      echo Excluindo diretorio %%a\Dados\BinarioPlano
      rd /s /q %%a\Dados\BinarioPlano
@@ -16,21 +16,14 @@ FOR /D %%a in (Q:\*,
      echo Excluindo diretorio %%a\Dados\BinarioOcorrenciaReceita
      rd /s /q %%a\Dados\BinarioOcorrenciaReceita
 
-     FOR /D %%c in (%%a\Versao\*) DO (
+     FOR /D %%c in (%%a\Versao\*,
+                    %%a\VersaoPlano\*,
+                    %%a\Qualidade\* ) DO (
 	  echo Excluindo diretorio %%c\dashboard
           rd /s /q %%c\dashboard
 
 	  echo Excluindo diretorio %%c\ReportBuilder
           rd /s /q %%c\ReportBuilder
-     )
-
-
-     FOR /D %%d in (%%a\Qualidade\*) DO (
-	  echo Excluindo diretorio %%d\dashboard
-          rd /s /q %%d\dashboard
-
-	  echo Excluindo diretorio %%d\ReportBuilder
-          rd /s /q %%d\ReportBuilder
      )
 )
 
