2ad1f2e091e58cc451587533ea78716d326e3a2c
lucas.stein <stein.lucas@icloud.com>
2023-10-05 16:07:02 -0300

    feature

--- ExcluirDiretoriosInutilizados.bat
+++ ExcluirDiretoriosInutilizados.bat
@@ -1,26 +1,37 @@
 @echo off
 
-FOR /D %%a in (*) DO (
-     echo Excluindo diretorio %cd%\%%a\Dados\BinarioPlano
-     rd /s /q %cd%\%%a\Dados\BinarioPlano
+FOR /D %%a in (Q:\*,
+	       R:\SGT\*,
+               R:\Process\*,
+               R:\ORACLE19\SGT\*,
+               R:\ORACLE19\Process\*,
+               P:\Cliente\Ora\*) DO (
 
-     echo Excluindo diretorio %cd%\%%a\Dados\BinarioEfetivacao
-     rd /s /q %cd%\%%a\Dados\BinarioEfetivacao
+     echo Excluindo diretorio %%a\Dados\BinarioPlano
+     rd /s /q %%a\Dados\BinarioPlano
 
-     echo Excluindo diretorio %cd%\%%a\Dados\BinarioOcorrenciaReceita
-     rd /s /q %cd%\%%a\Dados\BinarioOcorrenciaReceita
-)
+     echo Excluindo diretorio %%a\Dados\BinarioEfetivacao
+     rd /s /q %%a\Dados\BinarioEfetivacao
 
-FOR /D %%b in (*) DO (
-    cd %cd%\%%b\versao
+     echo Excluindo diretorio %%a\Dados\BinarioOcorrenciaReceita
+     rd /s /q %%a\Dados\BinarioOcorrenciaReceita
 
-    FOR /D %%c in (*00.*) DO (
-	echo Excluindo diretorio %cd%\%%b\versao\%%c\dashboard
-	rd /s /q %cd%\%%b\versao\%%c\dashboard
+     FOR /D %%c in (%%a\Versao\*) DO (
+	  echo Excluindo diretorio %%c\dashboard
+          rd /s /q %%c\dashboard
 
-	echo Excluindo diretorio %cd%\%%b\versao\%%c\ReportBuilder
-        rd /s /q %cd%\%%b\versao\%%c\ReportBuilder
-    )
+	  echo Excluindo diretorio %%c\ReportBuilder
+          rd /s /q %%c\ReportBuilder
+     )
+
+
+     FOR /D %%d in (%%a\Qualidade\*) DO (
+	  echo Excluindo diretorio %%d\dashboard
+          rd /s /q %%d\dashboard
+
+	  echo Excluindo diretorio %%d\ReportBuilder
+          rd /s /q %%d\ReportBuilder
+     )
 )
 
 pause
