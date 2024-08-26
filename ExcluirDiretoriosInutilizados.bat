b9644469aecce7269eea6bad2d920f81f93a96a7
lucas.stein <stein.lucas@icloud.com>
2024-08-26 08:54:10 -0300

    Update ExcluirDiretoriosInutilizados.bat

--- ExcluirDiretoriosInutilizados.bat
+++ ExcluirDiretoriosInutilizados.bat
@@ -18,16 +18,6 @@ FOR /D %%a in (Q:\*,
 
      echo Excluindo diretorio %%a\Dados\BinarioIntegracao
      rd /s /q %%a\Dados\BinarioIntegracao
-
-     FOR /D %%c in (%%a\Versao\*,
-                    %%a\VersaoPlano\*,
-                    %%a\Qualidade\* ) DO (
-	  echo Excluindo diretorio %%c\dashboard
-          rd /s /q %%c\dashboard
-
-	  echo Excluindo diretorio %%c\ReportBuilder
-          rd /s /q %%c\ReportBuilder
-     )
 )
 
 pause
